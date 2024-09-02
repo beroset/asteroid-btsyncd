4dd5cbf3eba401d9fdbcc8db73b420b0381002de
Ed Beroset <beroset@ieee.org>
2024-09-02 16:35:21 -0400

    Create object for heartrate monitor
    The run() method doesn't really work since the member variables go out
    of scope immediately, but it's a start.

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,61 @@
 #include <QtCore/qtimer.h>
 #include <QTranslator>
 
+class BtService {
+public:
+    BtService();
+    const QLowEnergyServiceData& service() const;
+    void run(QLowEnergyService& service);
+private:
+    QLowEnergyCharacteristicData charData;
+    QLowEnergyServiceData serviceData;
+};
+
+BtService::BtService() 
+{
+    charData.setUuid(QBluetoothUuid::HeartRateMeasurement);
+    charData.setValue(QByteArray(2, 0));
+    charData.setProperties(QLowEnergyCharacteristic::Notify | QLowEnergyCharacteristic::Read);
+    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::ClientCharacteristicConfiguration,
+                                                QByteArray(2, 0));
+    charData.addDescriptor(clientConfig);
+
+    serviceData.setType(QLowEnergyServiceData::ServiceTypePrimary);
+    serviceData.setUuid(QBluetoothUuid::HeartRate);
+    serviceData.addCharacteristic(charData);
+}
+
+const QLowEnergyServiceData& BtService::service() const
+{
+    return serviceData;
+}
+
+void BtService::run(QLowEnergyService& service)
+{
+    QTimer heartbeatTimer;
+    quint8 currentHeartRate = 60;
+    enum ValueChange { ValueUp, ValueDown } valueChange = ValueUp;
+    const auto heartbeatProvider = [&service, &currentHeartRate, &valueChange]() {
+        QByteArray value;
+        value.append(char(0));
+        value.append(char(currentHeartRate)); // Actual value.
+        QLowEnergyCharacteristic characteristic
+                = service.characteristic(QBluetoothUuid::HeartRateMeasurement);
+        Q_ASSERT(characteristic.isValid());
+        service.writeCharacteristic(characteristic, value); // Potentially causes notification.
+        if (currentHeartRate == 60)
+            valueChange = ValueUp;
+        else if (currentHeartRate == 100)
+            valueChange = ValueDown;
+        if (valueChange == ValueUp)
+            ++currentHeartRate;
+        else
+            --currentHeartRate;
+    };
+    QObject::connect(&heartbeatTimer, &QTimer::timeout, heartbeatProvider);
+    heartbeatTimer.start(1000);
+}
+
 int main(int argc, char *argv[])
 {
     //QLoggingCategory::setFilterRules(QStringLiteral("qt.bluetooth* = true"));
@@ -54,55 +109,23 @@ int main(int argc, char *argv[])
     //! [Advertising Data]
 
     //! [Service Data]
-    QLowEnergyCharacteristicData charData;
-    charData.setUuid(QBluetoothUuid::HeartRateMeasurement);
-    charData.setValue(QByteArray(2, 0));
-    charData.setProperties(QLowEnergyCharacteristic::Notify);
-    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::ClientCharacteristicConfiguration,
-                                                QByteArray(2, 0));
-    charData.addDescriptor(clientConfig);
-
-    QLowEnergyServiceData serviceData;
-    serviceData.setType(QLowEnergyServiceData::ServiceTypePrimary);
-    serviceData.setUuid(QBluetoothUuid::HeartRate);
-    serviceData.addCharacteristic(charData);
+    BtService svc;
     //! [Service Data]
 
     //! [Start Advertising]
     const QScopedPointer<QLowEnergyController> leController(QLowEnergyController::createPeripheral());
-    QScopedPointer<QLowEnergyService> service(leController->addService(serviceData));
+    QScopedPointer<QLowEnergyService> service(leController->addService(svc.service()));
     leController->startAdvertising(QLowEnergyAdvertisingParameters(), advertisingData,
                                    advertisingData);
     //! [Start Advertising]
 
     //! [Provide Heartbeat]
-    QTimer heartbeatTimer;
-    quint8 currentHeartRate = 60;
-    enum ValueChange { ValueUp, ValueDown } valueChange = ValueUp;
-    const auto heartbeatProvider = [&service, &currentHeartRate, &valueChange]() {
-        QByteArray value;
-        value.append(char(0));
-        value.append(char(currentHeartRate)); // Actual value.
-        QLowEnergyCharacteristic characteristic
-                = service->characteristic(QBluetoothUuid::HeartRateMeasurement);
-        Q_ASSERT(characteristic.isValid());
-        service->writeCharacteristic(characteristic, value); // Potentially causes notification.
-        if (currentHeartRate == 60)
-            valueChange = ValueUp;
-        else if (currentHeartRate == 100)
-            valueChange = ValueDown;
-        if (valueChange == ValueUp)
-            ++currentHeartRate;
-        else
-            --currentHeartRate;
-    };
-    QObject::connect(&heartbeatTimer, &QTimer::timeout, heartbeatProvider);
-    heartbeatTimer.start(1000);
+    svc.run(*service);
     //! [Provide Heartbeat]
 
-    auto reconnect = [&leController, advertisingData, &service, serviceData]()
+    auto reconnect = [&leController, advertisingData, &service, &svc]()
     {
-        service.reset(leController->addService(serviceData));
+        service.reset(leController->addService(svc.service()));
         if (!service.isNull())
             leController->startAdvertising(QLowEnergyAdvertisingParameters(),
                                            advertisingData, advertisingData);
