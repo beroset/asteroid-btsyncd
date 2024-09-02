a8c155cf91474b7fdbfea56fe4102a818f42af6d
Ed Beroset <beroset@ieee.org>
2024-09-02 12:51:12 -0400

    Experimental heartrate build

--- src/main.cpp
+++ src/main.cpp
@@ -15,43 +15,99 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <QtBluetooth/qlowenergyadvertisingdata.h>
+#include <QtBluetooth/qlowenergyadvertisingparameters.h>
+#include <QtBluetooth/qlowenergycharacteristic.h>
+#include <QtBluetooth/qlowenergycharacteristicdata.h>
+#include <QtBluetooth/qlowenergydescriptordata.h>
+#include <QtBluetooth/qlowenergycontroller.h>
+#include <QtBluetooth/qlowenergyservice.h>
+#include <QtBluetooth/qlowenergyservicedata.h>
 
-#include <QtCore/QCoreApplication>
-#include <QtDBus/QtDBus>
+#include <QBluetoothUuid>
+#include <QLowEnergyService>
+#include <QLowEnergyCharacteristicData>
 
-#include "common.h"
-#include "application.h"
-#include "advertisement.h"
-#include "bluezmanager.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qcoreapplication.h>
+#include <QtCore/qlist.h>
+#include <QtCore/qloggingcategory.h>
+#include <QtCore/qscopedpointer.h>
+#include <QtCore/qtimer.h>
+#include <QTranslator>
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-    QCoreApplication qcoreapp(argc, argv);
+    //QLoggingCategory::setFilterRules(QStringLiteral("qt.bluetooth* = true"));
+#ifndef Q_OS_ANDROID
+    QCoreApplication app(argc, argv);
+#else
+    QGuiApplication app(argc, argv);
+#endif
 
-    QTranslator translator;
-    translator.load(QLocale(), "asteroid-btsyncd", ".", "/usr/share/translations", ".qm");
-    qcoreapp.installTranslator(&translator);
+    //! [Advertising Data]
+    QLowEnergyAdvertisingData advertisingData;
+    advertisingData.setDiscoverability(QLowEnergyAdvertisingData::DiscoverabilityGeneral);
+    advertisingData.setIncludePowerLevel(true);
+    advertisingData.setLocalName("catfish3");
+    advertisingData.setServices(QList<QBluetoothUuid>() << QBluetoothUuid::HeartRate);
+    //! [Advertising Data]
 
-    if (!QDBusConnection::systemBus().isConnected()) {
-        fprintf(stderr, "Cannot connect to the D-Bus system bus.\n");
-        return 3;
-    }
-    if (!QDBusConnection::sessionBus().isConnected()) {
-        fprintf(stderr, "Cannot connect to the D-Bus session bus.\n");
-        return 2;
-    }
-    /* if (!bus.registerService(SERVICE_NAME)) {
-        fprintf(stderr, "%s\n",
-                qPrintable(QDBusConnection::systemBus().lastError().message()));
-        exit(1);
-    } */
+    //! [Service Data]
+    QLowEnergyCharacteristicData charData;
+    charData.setUuid(QBluetoothUuid::HeartRateMeasurement);
+    charData.setValue(QByteArray(2, 0));
+    charData.setProperties(QLowEnergyCharacteristic::Notify);
+    const QLowEnergyDescriptorData clientConfig(QBluetoothUuid::ClientCharacteristicConfiguration,
+                                                QByteArray(2, 0));
+    charData.addDescriptor(clientConfig);
 
-    Application app;
-    Advertisement advert;
-    BlueZManager bm(app.getPath(), advert.getPath());
+    QLowEnergyServiceData serviceData;
+    serviceData.setType(QLowEnergyServiceData::ServiceTypePrimary);
+    serviceData.setUuid(QBluetoothUuid::HeartRate);
+    serviceData.addCharacteristic(charData);
+    //! [Service Data]
 
-    qcoreapp.exec();
-    return 0;
+    //! [Start Advertising]
+    const QScopedPointer<QLowEnergyController> leController(QLowEnergyController::createPeripheral());
+    QScopedPointer<QLowEnergyService> service(leController->addService(serviceData));
+    leController->startAdvertising(QLowEnergyAdvertisingParameters(), advertisingData,
+                                   advertisingData);
+    //! [Start Advertising]
+
+    //! [Provide Heartbeat]
+    QTimer heartbeatTimer;
+    quint8 currentHeartRate = 60;
+    enum ValueChange { ValueUp, ValueDown } valueChange = ValueUp;
+    const auto heartbeatProvider = [&service, &currentHeartRate, &valueChange]() {
+        QByteArray value;
+        value.append(char(0));
+        value.append(char(currentHeartRate)); // Actual value.
+        QLowEnergyCharacteristic characteristic
+                = service->characteristic(QBluetoothUuid::HeartRateMeasurement);
+        Q_ASSERT(characteristic.isValid());
+        service->writeCharacteristic(characteristic, value); // Potentially causes notification.
+        if (currentHeartRate == 60)
+            valueChange = ValueUp;
+        else if (currentHeartRate == 100)
+            valueChange = ValueDown;
+        if (valueChange == ValueUp)
+            ++currentHeartRate;
+        else
+            --currentHeartRate;
+    };
+    QObject::connect(&heartbeatTimer, &QTimer::timeout, heartbeatProvider);
+    heartbeatTimer.start(1000);
+    //! [Provide Heartbeat]
+
+    auto reconnect = [&leController, advertisingData, &service, serviceData]()
+    {
+        service.reset(leController->addService(serviceData));
+        if (!service.isNull())
+            leController->startAdvertising(QLowEnergyAdvertisingParameters(),
+                                           advertisingData, advertisingData);
+    };
+    QObject::connect(leController.data(), &QLowEnergyController::disconnected, reconnect);
+
+    return app.exec();
 }
