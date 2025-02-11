dbad3cd7ba8a304f4f43d0be27f71b2b7a131aaa
Ed Beroset <beroset@ieee.org>
2025-02-11 15:34:19 -0500

    Add Pairing and reset functionality
    This is a start, but not everything works after disconnect/reconnect.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/BluetoothService.cpp
+++ src/BluetoothService.cpp
@@ -7,10 +7,25 @@ static const QBluetoothUuid AsteroidOSUuid{QString{"00000000-0000-0000-0000-00a5
 
 BluetoothService::BluetoothService(QObject *parent) : QObject(parent) {
     m_controller = QLowEnergyController::createPeripheral(this);
+    m_local = new QBluetoothLocalDevice(this);
 
     connect(m_controller, &QLowEnergyController::stateChanged, this, &BluetoothService::onControllerStateChanged);
     connect(m_controller, QOverload<QLowEnergyController::Error>::of(&QLowEnergyController::error), this, &BluetoothService::onErrorOccurred);
+    connect(m_local, &QBluetoothLocalDevice::pairingFinished, this, &BluetoothService::onPairingFinished);
 }
+
+void BluetoothService::reset() {
+    // how should this work?  If I disconnect and reconnect,
+    // subsequent tries to read, say, the battery level gives 
+    // "The handle is invalid" on the phone from nRF connect.
+    m_controller = QLowEnergyController::createPeripheral(this);
+    m_local = new QBluetoothLocalDevice(this);
+
+    connect(m_controller, &QLowEnergyController::stateChanged, this, &BluetoothService::onControllerStateChanged);
+    connect(m_controller, QOverload<QLowEnergyController::Error>::of(&QLowEnergyController::error), this, &BluetoothService::onErrorOccurred);
+    connect(m_local, &QBluetoothLocalDevice::pairingFinished, this, &BluetoothService::onPairingFinished);
+}
+
 
 QLowEnergyService* BluetoothService::addService(const QLowEnergyServiceData &serviceData) {
     QLowEnergyService *service = m_controller->addService(serviceData);
@@ -30,29 +45,47 @@ void BluetoothService::startAdvertising(const QString &localName) {
     }
     advertisingData.setServices(serviceUuids);
 #else
-    advertisingData.setServices(QList<QBluetoothUuid>() << AsteroidOSUuid);
+    QList<QBluetoothUuid> serviceUuids;
+    serviceUuids.append(AsteroidOSUuid);
+    advertisingData.setServices(serviceUuids);
 #endif
 
     QLowEnergyAdvertisingParameters advertisingParameters{};
     advertisingParameters.setMode(QLowEnergyAdvertisingParameters::AdvInd);
 
-    m_controller->startAdvertising(advertisingParameters, advertisingData, advertisingData);
+    m_controller->startAdvertising(advertisingParameters, advertisingData);
 }
 
 void BluetoothService::onControllerStateChanged(QLowEnergyController::ControllerState state) {
     if (state == QLowEnergyController::UnconnectedState) {
         qDebug() << "Peripheral disconnected.";
+        m_local->requestPairing(m_controller->remoteAddress(), QBluetoothLocalDevice::Unpaired);
+        reset();
+        emit deviceDisconnected();
     } else if (state == QLowEnergyController::AdvertisingState) {
         qDebug() << "Advertising started.";
     } else if (state == QLowEnergyController::ConnectedState) {
-        qDebug() << "Connected state -- look for ANCS";
+        qDebug() << "Connected state";
+        pairDevice(m_controller->remoteAddress());
         emit deviceConnected(m_controller->remoteAddress(), m_controller->localAddress());
     } else {
         qDebug() << "BluetoothService is in state " << state;
     }
 }
 
+void BluetoothService::pairDevice(const QBluetoothAddress &address) {
+    if (m_local->pairingStatus(address) == QBluetoothLocalDevice::Paired || 
+        m_local->pairingStatus(address) == QBluetoothLocalDevice::AuthorizedPaired) {
+        return;
+    }
+    m_local->requestPairing(m_controller->remoteAddress(), QBluetoothLocalDevice::Paired);
+}
+
 void BluetoothService::onErrorOccurred(QLowEnergyController::Error error) {
     qWarning() << "Error occurred:" << error;
+}
+
+void BluetoothService::onPairingFinished(const QBluetoothAddress &address, QBluetoothLocalDevice::Pairing pairing) {
+    qDebug() << address << pairing;
 }
 
