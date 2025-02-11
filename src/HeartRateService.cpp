b253f76f9b6116a72c7e3d6832d421a60592ee35
Ed Beroset <beroset@ieee.org>
2025-02-11 15:41:33 -0500

    Fix format of heart rate data
    The format of the heart rate data contains a flag byte as well as the
    8-bit value.  This effects that change.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/HeartRateService.cpp
+++ src/HeartRateService.cpp
@@ -18,13 +18,24 @@ void HeartRateService::onStatusChanged(QHrmSensor::Status status) {
     qDebug() << "HRM status is now " << labels[status] << ", value = " << value << " bpm";
     QLowEnergyCharacteristic characteristic = m_service->characteristic(QBluetoothUuid::HeartRateMeasurement);
     Q_ASSERT(characteristic.isValid());
-    m_service->writeCharacteristic(characteristic, QByteArray(1, value));
+    QByteArray bytes{};
+    char constexpr skin_contact_feature{0x4};
+    char constexpr skin_contact_detected{0x6};
+    char flag{status ? skin_contact_detected : skin_contact_feature};
+    bytes.append(flag);  // flag value
+    bytes.append(value);
+    m_service->writeCharacteristic(characteristic, bytes);
 }
 
 QLowEnergyServiceData HeartRateService::createHeartRateServiceData() {
     QLowEnergyCharacteristicData heartRateMeasurementData;
     heartRateMeasurementData.setUuid(QBluetoothUuid::HeartRateMeasurement);
-    heartRateMeasurementData.setValue(QByteArray(1, 60)); // 60 bpm heart rate
+    QByteArray value{};
+    char constexpr skin_contact_feature{0x4};
+    char flag{skin_contact_feature};
+    value.append(flag);
+    value.append(60);  // 60 bpm
+    heartRateMeasurementData.setValue(value); // 0 bpm heart rate
     heartRateMeasurementData.setProperties(QLowEnergyCharacteristic::Read | QLowEnergyCharacteristic::Notify);
 
     QLowEnergyDescriptorData cccd(QBluetoothUuid::ClientCharacteristicConfiguration, QByteArray(2, 0));
