493e4136cf851df4cff7b3d00875cd6ceb3de71b
Ed Beroset <beroset@ieee.org>
2025-02-11 15:35:21 -0500

    Add support for pairing and disconnect/reconnect
    Not everything works yet, but it's a start.
    
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/BluetoothService.h
+++ src/BluetoothService.h
@@ -1,11 +1,13 @@
 #ifndef BLUETOOTHSERVICE_H
 #define BLUETOOTHSERVICE_H
 
+#include "asteroid-btsyncd.h"
 #include <QObject>
 #include <QtBluetooth/QLowEnergyController>
 #include <QtBluetooth/QLowEnergyService>
 #include <QtBluetooth/QLowEnergyCharacteristic>
 #include <QtBluetooth/QLowEnergyDescriptor>
+#include <QBluetoothLocalDevice>
 #include <QLoggingCategory>
 
 Q_DECLARE_LOGGING_CATEGORY(btsyncd)
@@ -20,13 +22,18 @@ public:
 
 signals:
     void deviceConnected(QBluetoothAddress remote, QBluetoothAddress local);
+    void deviceDisconnected();
 
 private slots:
     void onControllerStateChanged(QLowEnergyController::ControllerState state);
     void onErrorOccurred(QLowEnergyController::Error error);
+    void onPairingFinished(const QBluetoothAddress &address, QBluetoothLocalDevice::Pairing pairing);
 
 private:
+    void pairDevice(const QBluetoothAddress &address);
+    void reset();
     QLowEnergyController *m_controller = nullptr;
+    QBluetoothLocalDevice *m_local = nullptr;
     QList<QLowEnergyService *> m_services;
 };
 
