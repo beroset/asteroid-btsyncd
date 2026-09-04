9176707bfd8a79eae559c9f48b2e0039826f94c5
Ed Beroset <beroset@ieee.org>
2026-09-04 15:17:13 -0400

    Only install translater if it loaded successfully
    Signed-off-by: Ed Beroset <beroset@ieee.org>

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,6 @@
 #include <QtCore/QCoreApplication>
 #include <QtDBus/QtDBus>
 
-#include "common.h"
 #include "application.h"
 #include "advertisement.h"
 #include "bluezmanager.h"
@@ -31,8 +30,9 @@ int main(int argc, char **argv)
     QCoreApplication qcoreapp(argc, argv);
 
     QTranslator translator;
-    translator.load(QLocale(), "asteroid-btsyncd", ".", "/usr/share/translations", ".qm");
-    qcoreapp.installTranslator(&translator);
+    if (translator.load(QLocale(), "asteroid-btsyncd", ".", "/usr/share/translations", ".qm")) {
+        qcoreapp.installTranslator(&translator);
+    }
 
     if (!QDBusConnection::systemBus().isConnected()) {
         fprintf(stderr, "Cannot connect to the D-Bus system bus.\n");
